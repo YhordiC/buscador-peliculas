0b13a2811e8f5b11a0227ad0e10aa193468c4056
Yhordi Choque <yordychoque124@gmail.com>
2023-11-14 11:05:28 -0400

    input y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,19 @@ function App() {
   
 
   return (
-    <>
-    Hola Mundo
-    </>
+    <div className='container-web'>
+    <h1>Hola Mundo</h1>
+    <header>
+      <form className='form'>
+        <input type='text' placeholder='Avengers,Star Wars, The Matrix...'/>
+        <button type='submit'>Buscar</button>
+      </form>
+    </header>
+
+    <main>
+      Aqui iran las peliculas
+    </main>
+    </div>
   )
 }
 
