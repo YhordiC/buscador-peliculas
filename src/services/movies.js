c83ba9644ffe38d0c4e87f816defdb786204c88e
Yhordi Choque <yordychoque124@gmail.com>
2023-11-22 10:53:53 -0400

    cree un movie para hacer la solicitud a la api,  con funcion asincrona y promesas

---
+++ src/services/movies.js
@@ -0,0 +1,22 @@
+const API_KEY = 'd67859d2'
+
+export async function searchMovies  (search) {
+    if(search === '' ) return null; 
+        //setResponseMovies(withResults)
+    try {
+        const response  = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+        const json = await response.json()
+     
+        const movies = json.Search
+
+        return  movies?.map(movie => (
+            { id: movie.imdbID,
+              title:movie.Title,
+              year:movie.Year,
+              poster:movie.Poster,
+            } ))
+    } catch(error){
+        throw new Error('Error searching for movies')
+    }
+    }
+    
